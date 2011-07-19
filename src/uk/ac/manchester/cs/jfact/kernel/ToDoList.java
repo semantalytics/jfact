a2420f2d2914e345c6b197ce7c2520b347b0efd4
ignazio1977 <ignazio1977@0c4df381-a941-41be-a109-d13c7488f84d>
2011-07-19 14:16:25 +0000

    fix for an intermittent null pointer exception - seems to be caused by the thread creating instances of ToDoListSaveState

--- src/uk/ac/manchester/cs/jfact/kernel/ToDoList.java
+++ src/uk/ac/manchester/cs/jfact/kernel/ToDoList.java
@@ -2,7 +2,7 @@ package uk.ac.manchester.cs.jfact.kernel;
 
 /* This file is part of the JFact DL reasoner
 Copyright 2011 by Ignazio Palmisano, Dmitry Tsarkov, University of Manchester
-This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version. 
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
 This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA*/
 import static uk.ac.manchester.cs.jfact.helpers.LeveLogger.logger;
@@ -234,9 +234,9 @@ public final class ToDoList {
 		protected int backupID_sp;
 		protected int backupID_ep;
 		/** save state for queueNN */
-		protected QueueQueueSaveState backupNN = new QueueQueueSaveState();
+		protected final QueueQueueSaveState backupNN = new QueueQueueSaveState();
 		/** save state of all regular queues */
-		protected int[][] backup = new int[nRegularOptions][2];
+		protected final int[][] backup = new int[nRegularOptions][2];
 		/** save number-of-entries to do */
 		protected int noe;
 
