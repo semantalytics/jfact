db04e84ab197f122aea17a9e06238589fae123cd
ignazio1977 <ignazio1977@0c4df381-a941-41be-a109-d13c7488f84d>
2012-01-23 11:43:40 +0000

    renamed new datatypes package

--- datatypes/datatypes/NumericLiteral.java
+++
@@ -1,4 +0,0 @@
-package datatypes;
-
-/** all literals whose datatype is numeric */
-public interface NumericLiteral<R extends Comparable<R>> extends Literal<R> {}
--- datatypes/datatypes/NumericLiteralImpl.java
+++
@@ -1,9 +0,0 @@
-package datatypes;
-
-/** all literals whose datatype is numeric */
-public class NumericLiteralImpl<R extends Comparable<R>> extends LiteralImpl<R> implements
-		NumericLiteral<R> {
-	public NumericLiteralImpl(NumericDatatype<R> type, String value) {
-		super(type, value);
-	}
-}
